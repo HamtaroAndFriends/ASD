acdb2fca2856dbe19cf7683ce2a3a1769d906050
Samuele Colombo <samuele.colombo91@hotmail.it>
2016-03-16 09:57:23 +0100

    Added support for SyncAutoma into ControllerJSON

--- src/controller/js/ControllerJSON.java
+++ src/controller/js/ControllerJSON.java
@@ -13,6 +13,9 @@ import java.util.stream.Collectors;
 import model.Automa;
 import model.State;
 import model.Transition;
+import model.sync.SyncAutoma;
+import model.sync.SyncState;
+import model.sync.SyncTransition;
 
 
 /**
@@ -81,6 +84,25 @@ public class ControllerJSON
      * @param automa
      * @return 
      */
+    public String getStateJSON(SyncAutoma automa)
+    {
+        StringBuilder json = new StringBuilder();
+        
+        json.append(OPEN_STATE + OPEN_SQUARE);
+        
+        for(SyncState s : automa.getStates())
+        {
+            json.append(String.format(JSON_STATE, s.getState1().getName() + s.getState2().getName())).append(COMMA);
+        }
+
+        return json.toString();
+    }
+    
+    /**
+     * 
+     * @param automa
+     * @return 
+     */
     public String getTransitionJSON(Automa automa)
     {
         StringBuilder json = new StringBuilder();
@@ -90,6 +112,29 @@ public class ControllerJSON
         for(Transition t : automa.getTransitions())
         {
             json.append(String.format(JSON_TRANSITION, t.getStart().getName(), t.getEnd().getName(), Arrays.toString(t.getEvent().getEvents().toArray()), (t.isFault() ? "red" : "blue"))).append(COMMA);
+        }
+        
+        return json.toString();
+
+    }
+    
+    /**
+     * 
+     * @param automa
+     * @return 
+     */
+    public String getTransitionJSON(SyncAutoma automa)
+    {
+        StringBuilder json = new StringBuilder();
+        
+        json.append(OPEN_TRANSITION + OPEN_SQUARE);
+        
+        for(SyncTransition t : automa.getTransitions())
+        {
+            json.append(String.format(JSON_TRANSITION, 
+                    t.getStart().getState1().getName() + t.getStart().getState2().getName(), 
+                    t.getEnd().getState1().getName() + t.getEnd().getState2().getName(),
+                    Arrays.toString(t.getT1().getEvent().getEvents().toArray()), (t.isAmbiguous() ? "red" : "blue"))).append(COMMA);
         }
         
         return json.toString();
@@ -129,7 +174,63 @@ public class ControllerJSON
         
     }
     
+    /**
+     * 
+     * @param automa
+     * @return 
+     */
+    public String getAutomaJSON(SyncAutoma automa)
+    {
+        StringBuilder sb = new StringBuilder();
+        StringBuilder states = new StringBuilder(getStateJSON(automa));
+        StringBuilder transitions = new StringBuilder(getTransitionJSON(automa));
+        
+        for(SyncTransition t : automa.getTransitions().stream().filter((t) -> (t.getStart().equals(t.getEnd()))).collect(Collectors.toList()))
+        {
+            states.append(String.format(JSON_STATE, String.format(JSON_LOOP, t.getStart().getState1().getName() + t.getStart().getState2().getName()))).append(COMMA);
+            transitions.append(String.format(JSON_TRANSITION, t.getStart().getState1().getName() + t.getStart().getState2().getName(), String.format(JSON_LOOP, t.getEnd().getState1().getName() + t.getEnd().getState2().getName()), Arrays.toString(t.getT1().getEvent().getEvents().toArray()), (t.isAmbiguous()? "red" : "blue"))).append(COMMA);
+            transitions.append(String.format(JSON_TRANSITION, String.format(JSON_LOOP, t.getEnd().getState1().getName() + t.getEnd().getState2().getName()), t.getStart().getState1().getName() + t.getStart().getState2().getName(), Arrays.toString(t.getT1().getEvent().getEvents().toArray()), (t.isAmbiguous()? "red" : "blue"))).append(COMMA);
+            
+        }
+        states.deleteCharAt(states.length() - 1);
+        states.append(CLOSE_SQUARE);
+        
+        transitions.deleteCharAt(transitions.length() - 1);
+        transitions.append(CLOSE_SQUARE);
+        
+        sb.append(OPEN_BRACKET)
+            .append(states)
+            .append(COMMA)  
+            .append(transitions)
+            .append(CLOSE_BRACKET);
+        
+        return sb.toString();
+        
+    }
+    
+    /**
+     * 
+     * @param automa
+     * @param path
+     * @throws FileNotFoundException 
+     */
     public void createAutomaView(Automa automa, String path) throws FileNotFoundException
+    {
+        File file = new File(path + "\\ControllerAutoma.html");
+        String data = getAutomaJSON(automa);
+        PrintWriter pw = new PrintWriter(file);
+        pw.print(String.format(JSON_CODE, data));
+        pw.flush();
+        
+    }
+    
+    /**
+     * 
+     * @param automa
+     * @param path
+     * @throws FileNotFoundException 
+     */
+    public void createAutomaView(SyncAutoma automa, String path)throws FileNotFoundException
     {
         File file = new File(path + "\\ControllerAutoma.html");
         String data = getAutomaJSON(automa);
